6d20f647fe7c73081eba7d29295a0d0b248b6ef0
Mauricio Arias R <marias@lsv-tech.com>
2024-06-01 15:05:36 -0500

    refactor(models): add docstrings and reorder functions for better readability

--- fakestore_product_integration/models/product_template.py
+++ fakestore_product_integration/models/product_template.py
@@ -2,7 +2,8 @@ from odoo import models, fields, api
 import requests
 from odoo.exceptions import ValidationError
 import base64
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
@@ -11,7 +12,7 @@ class ProductTemplate(models.Model):
     rating_count = fields.Integer(string="Rating Count")
     image_url = fields.Char("Image URL")
     api_product = fields.Boolean(default=False)
-    custom_id = fields.Char(string='My Custom ID')
+    custom_id = fields.Char(string='Custom ID')
 
     @api.onchange("image_1920")
     def _onchange_image_1920(self):
@@ -19,7 +20,9 @@ class ProductTemplate(models.Model):
         Clear the image_url field when image_1920 is changed.
 
         This method is triggered whenever the image_1920 field is modified.
-        If image_1920 has a value, it clears the image_url field.
+        If image_1920 has a value, it resets the image_url field to indicate
+        that the product image has been edited. This allows the scheduled action
+        to know that the image needs to be updated with data from the API.
         """
         if self.image_1920:
             self.image_url = False
@@ -85,36 +88,16 @@ class ProductTemplate(models.Model):
             )
         return response.json()
 
-    def _get_or_create_category(self, category_name):
-        category = self.env["product.category"].search(
-            [("name", "=", category_name)], limit=1
-        )
-        if not category:
-            category = self.env["product.category"].create({"name": category_name})
-        return category
-
-    def _prepare_product_values(self, product_data, category):
-        rating_data = product_data.get("rating", {})
-        values = {
-            "custom_id":  product_data.get("id", "Default id"),
-            "name": product_data.get("title", "Default Title"),
-            "list_price": product_data.get("price", 0.0),
-            "categ_id": category.id,
-            "description_sale": product_data.get("description", ""),
-            "rating_rate": rating_data.get("rate", 0.0),
-            "rating_count": rating_data.get("count", 0.0),
-            "api_product": True,
-            "image_url": product_data.get("image"),
-        }
-        return values
-
-    def _update_product_image(self, product, new_image_url):
-        current_image_url = product.image_url
-        if current_image_url != new_image_url:
-            return self.get_image_from_url(new_image_url)
-        return None
-
     def _create_or_update_product(self, product_data):
+        """
+        Create or update a product record based on provided data from an API.
+
+        Args:
+            product_data (dict): Dictionary containing product data retrieved from an external API.
+
+        Returns:
+            None
+        """
         category = self._get_or_create_category(product_data.get("category"))
         product = self.env["product.template"].search(
             [("custom_id", "=", str(product_data.get("id")))], limit=1
@@ -131,7 +114,6 @@ class ProductTemplate(models.Model):
                     if new_image:
                         values["image_1920"] = new_image
                 except ValidationError as e:
-                    # Log or handle the error as needed
                     _logger.error(f"Error updating image for product {product_data.get('id')}: {e}")
             product.with_context(lang=self.env.user.lang).write(values)
             product.write(values)
@@ -142,10 +124,55 @@ class ProductTemplate(models.Model):
                     if image_response:
                         values["image_1920"] = image_response
                 except ValidationError as e:
-                    # Log or handle the error as needed
                     _logger.error(f"Error downloading image for new product {product_data.get('id')}: {e}")
                     
             self.create(values)
+
+
+    def _get_or_create_category(self, category_name):
+        """
+        Retrieve an existing product category by name or create 
+        a new one if it does not exist.
+
+        Args:
+            category_name (str): The name of the category to search 
+            for or create.
+
+        Returns:
+            recordset: A recordset of the product category.
+        """
+        category = self.env["product.category"].search(
+            [("name", "=", category_name)], limit=1
+        )
+        if not category:
+            category = self.env["product.category"].create({"name": category_name})
+        return category
+
+    def _prepare_product_values(self, product_data, category):
+        """
+        Prepare the values needed to create or update a product record.
+
+        Args:
+            product_data (dict): Dictionary containing product data from an external source.
+            category (recordset): A recordset of the product category.
+
+        Returns:
+            dict: A dictionary of values to create or update a product record.
+        """
+        rating_data = product_data.get("rating", {})
+        values = {
+            "custom_id":  product_data.get("id", "Default id"),
+            "name": product_data.get("title", "Default Title"),
+            "list_price": product_data.get("price", 0.0),
+            "categ_id": category.id,
+            "description_sale": product_data.get("description", ""),
+            "rating_rate": rating_data.get("rate", 0.0),
+            "rating_count": rating_data.get("count", 0.0),
+            "api_product": True,
+            "image_url": product_data.get("image"),
+        }
+        return values
+
 
     def get_image_from_url(self, url: str):
         """
